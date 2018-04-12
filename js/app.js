0d68185e2f138558727cb835c149a4e455dd14f8
Dustin <dustin.gogoll@googlemail.com>
2018-04-12 14:46:14 +0200

    basic enemy movement added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(field = 0) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = - 100;
+    this.y = field * 100 + 50;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -11,6 +13,11 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.x += 50*dt;
+
+    if (this.x > 500) {
+      this.x = -100;
+    }
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -24,11 +31,37 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+  constructor() {
+    this.sprite = 'images/char-boy.png';
+    this.y = 400;
+    this.x = 200;
+  }
 
+  update() {
+
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput() {
+
+  }
+}
+
+function randomNumber(min, max) {
+  Math.floor((Math.random() * max) + min);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+let allEnemies = [];
+allEnemies.push(new Enemy());
+let player = new Player();
 
 
 
