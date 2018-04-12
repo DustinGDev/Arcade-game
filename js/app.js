bd6498f5f23a38cb8c5e5d795b3fb4ba804f8bca
Dustin <dustin.gogoll@googlemail.com>
2018-04-12 17:10:40 +0200

    Movement and spawn logic added

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
+//bug direction var
+let fromRight = "-rotated";
+
 // Enemies our player must avoid
-var Enemy = function(field = 0) {
+var Enemy = function(field = 0, index = allEnemies.length) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = - 100;
-    this.y = field * 100 + 50;
+    this.y = field * 83 + 68;
+    this.index = index;
+
+    //Sets the direction from where the bug will come from
+    if (field == 1) {
+      bugDirection = fromRight;
+      this.x = 500;
+      this.movement = -50;
+    }
+    else {
+      bugDirection = "";
+      this.x = -100;
+      this.movement = 50;
+    }
+
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = `images/enemy-bug${bugDirection}.png`;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x += 50*dt;
+    this.x += this.movement*dt;
 
-    if (this.x > 500) {
-      this.x = -100;
+    //Swaps the old bug with a new one at the end of screen
+    if ((this.x > 500 && this.movement == 50) || (this.x < -100 && this.movement == -50) ) {
+      allEnemies[this.index] = (new Enemy(randomNumber(0, 2), this.index));
     }
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -39,20 +56,51 @@ class Player {
   }
 
   update() {
+    let Enemies = allEnemies;
+    let allEnemyPositions = (() => {
+      let positions = [];
+      if (Array.isArray(allEnemies)) {
+        for (let value of allEnemies) {
+          positions.push([value.x, value.y]);
+        }
+        return positions;
+      }
+    })()
 
+    console.log(this.y);
+
+    for (let value of allEnemyPositions){
+      if(value[1] == this.y && this.x < value[0]+80 && this.x > value[0]-80) {
+        this.y = 400;
+        this.x = 200;
+      }
+    }
   }
 
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
-  handleInput() {
-
+  handleInput(key) {
+    if (key == 'left' && this.x > 0) {
+      this.x += -100;
+    }
+    else if (key == 'right' && this.x < 400) {
+      this.x += 100;
+    }
+    else if (key == 'up' && this.y > 0) {
+      this.y += -83;
+    }
+    else if (key == 'down' && this.y < 400) {
+      this.y += 83;
+    }
   }
 }
 
+//Function so produce a random number
 function randomNumber(min, max) {
-  Math.floor((Math.random() * max) + min);
+  let number = Math.floor((Math.random() * (max + 1)) + min);
+  return number;
 }
 
 // Now instantiate your objects.
@@ -60,7 +108,15 @@ function randomNumber(min, max) {
 // Place the player object in a variable called player
 
 let allEnemies = [];
-allEnemies.push(new Enemy());
+
+//Initial Enemy Spawn function
+const initiateEnemies = setInterval(()=> {
+  if (allEnemies.length < 4){
+    allEnemies.push(new Enemy(randomNumber(0, 2)));
+  }
+  else {clearInterval(initiateEnemies)}
+}, 3000)
+allEnemies.push(new Enemy(randomNumber(0, 2)));
 let player = new Player();
 
 
